010b2670f851d71d18414fddce7d16169199f4fa
krjin21 <krjin21@m.fudan.edu.cn>
2022-12-16 18:41:41 +0800

    updata
    Add comment

--- ball.py
+++ ball.py
@@ -3,23 +3,25 @@ from gurobipy import GRB
 import numpy as np
 import math
 
+
 def ball(e):
-    #e = 0.005
+    ### Compute the parameters
     Omega = math.sqrt(2 * math.log(1 / e, math.e))
     mu = np.array([1.05 + 0.3 * ((200 - i - 1) / 199) for i in range(199)])
     sigma = np.array([0.05 + 0.6 * ((200 - i) / 199) for i in range(199)])
 
+    ### Generate a model
     m = gp.Model('Box')
     m.setAttr('ModelSense', GRB.MAXIMIZE)
     m.Params.OutputFlag = 0
 
+    ### Add variables
     x = []
     z = []
     absz = []
     w = []
     R = m.addVar(0, GRB.INFINITY, 1, vtype=GRB.CONTINUOUS, name='R')
     sqrtw = m.addVar(0, GRB.INFINITY, vtype=GRB.CONTINUOUS, name='Sqrtw')
-    sumz = m.addVar(0, GRB.INFINITY, vtype=GRB.CONTINUOUS, name='Sumz')
     for i in range(199):
         x.append(m.addVar(lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, name='x{}'.format(i)))
         z.append(m.addVar(vtype=GRB.CONTINUOUS, name='z{}'.format(i)))
@@ -27,6 +29,7 @@ def ball(e):
         w.append(m.addVar(vtype=GRB.CONTINUOUS, name='w{}'.format(i)))
     x.append(m.addVar(lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, name='x199'))
 
+    ### Add Constraints
     cons = []
     cons.append(m.addConstr(gp.quicksum([i * i for i in w]) <= sqrtw * sqrtw, name='Sqrt_W'))
     for i in range(199):
@@ -38,12 +41,13 @@ def ball(e):
         gp.quicksum([mu[i] * x[i] for i in range(199)]) + 1.05 * x[199] - gp.quicksum(absz) - Omega * sqrtw >= R,
         name='RC2_Constraints'))
 
+    ### Solve the model
     m.update()
-    m.write('123.lp')
     m.optimize()
     x = [i.x for i in x]
-    R=R.x
+    R = R.x
 
-    return [R,x]
+    return [R, x]
 
-ball(0.005)
+
+R, x = ball(0.005)
--- budget.py
+++ budget.py
@@ -5,25 +5,23 @@ import numpy as np
 
 
 def budget(e):
-###计算参数
-    #e=0.005
-    gamma=math.sqrt(-2*math.log(e,math.e)*199)
+    ### Compute the parameters
+    gamma = math.sqrt(-2 * math.log(e, math.e) * 199)
     mu = np.array([1.05 + 0.3 * ((200 - i - 1) / 199) for i in range(199)])
     sigma = np.array([0.05 + 0.6 * ((200 - i) / 199) for i in range(199)])
 
-    ###建立模型
+    ### Generate a model
     m = gp.Model('Box')
     m.setAttr('ModelSense', GRB.MAXIMIZE)
     m.Params.OutputFlag = 0
 
-    ###生成变量
+    ### Add variables
     x = []
     z = []
     absz = []
     w = []
-    R = m.addVar(0, GRB.INFINITY,1, vtype=GRB.CONTINUOUS, name='R')
+    R = m.addVar(0, GRB.INFINITY, 1, vtype=GRB.CONTINUOUS, name='R')
     maxw = m.addVar(0, GRB.INFINITY, vtype=GRB.CONTINUOUS, name='Maxw')
-    sumz = m.addVar(0, GRB.INFINITY, vtype=GRB.CONTINUOUS, name='Sumz')
     for i in range(199):
         x.append(m.addVar(lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, name='x{}'.format(i)))
         z.append(m.addVar(vtype=GRB.CONTINUOUS, name='z{}'.format(i)))
@@ -31,25 +29,25 @@ def budget(e):
         w.append(m.addVar(vtype=GRB.CONTINUOUS, name='w{}'.format(i)))
     x.append(m.addVar(lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, name='x199'))
 
-
-    ###添加约束
+    ### Add Constraints
     cons = []
     for i in range(199):
         cons.append(m.addConstr(z[i] + w[i] == sigma[i] * x[i], name='RC_Constraints{}'.format(i)))
         cons.append(m.addConstr(z[i] <= absz[i], name='Abs1_z{}'.format(i)))
         cons.append(m.addConstr(z[i] >= -absz[i], name='Abs2_z{}'.format(i)))
-        cons.append(m.addConstr(w[i]<=maxw,name='Max_w{}'.format(i)))
+        cons.append(m.addConstr(w[i] <= maxw, name='Max_w{}'.format(i)))
     cons.append(m.addConstr(gp.quicksum(x) == 1, name='Money_Constraint'))
     cons.append(m.addConstr(
         gp.quicksum([mu[i] * x[i] for i in range(199)]) + 1.05 * x[199] - gp.quicksum(absz) - gamma * maxw >= R,
         name='RC2_Constraints'))
 
-    #模型求解
+    ### Solve the model
     m.update()
-    m.write('123.lp')
     m.optimize()
-    x=[i.x for i in x]
-    R=R.x
+    x = [i.x for i in x]
+    R = R.x
 
-    return [R,x]
+    return [R, x]
 
+
+R, x = budget(0.005)
